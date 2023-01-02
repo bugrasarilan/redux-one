03fba72710340140da14f5db1576a154d831eb07
bugrasarilan <bugrasarilan@gmail.com>
2023-01-02 22:25:51 +0300

    .

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,7 @@
 import { faker } from "@faker-js/faker/locale/en";
 
-// This file has nothing to do with Redux
-// It exports functions that create random
-// movies and song
-
+// create random
+// movies and songs
 export const createRandomMovie = () => {
   return `${faker.word.adjective()} ${faker.word.noun()}`;
 };
